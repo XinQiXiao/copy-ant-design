2c4d405ff689a65376f40d8e471fe5baecff4021
qixin <qixin@haoqix.com>
2018-07-06 11:25:52 +0800

    mock add

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 	A demo for learning from project of ant-design-pro.
 	url: https://github.com/ant-design/ant-design-pro.git 
 
-	###
+	### branch && 功能
+
+		## tag t0.1.0
+			构建项目框架
+
+		# tag t0.2.0
+			依赖 mockjs 
+						url: https://github.com/nuysoft/Mock
+					拦截请求，本地提供数据
+
+					roadhog-api-doc
+					配合 mockjs 提供数据
+
+					添加mock 文件， 添加 .roadhogrc.mock.js 文件
+			配置成功后，可在本地浏览器，发起模拟请求， 如localhost:8000/api/users/ 可看到数据
---
+++ mock/index.js
@@ -0,0 +1,23 @@
+
+import { getNotice, getFakeList, getActivities } from './api'
+import { getFakeChartData } from './chart'
+import { getNotices } from './notices'
+import { getProfileBasicData, getProfileAdvancedData } from './profile'
+import { getRule, postRule } from './rule'
+
+export {
+	// api
+	getNotice,
+	getFakeList,
+	getActivities,
+	// chart
+	getFakeChartData,
+	// notices
+	getNotices,
+	// profile
+	getProfileBasicData,
+	getProfileAdvancedData,
+	// rule
+	getRule, 
+	postRule,
+}
